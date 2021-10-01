1886f423a53d2cf1eef1fae2081db8b8c67240c5
Mahad <56235065+Mahad-lab@users.noreply.github.com>
2021-10-01 16:57:50 +0500

    project upload

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Aamir',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2021'
+    },
+    {
+        'author': 'Adnan',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2021'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
